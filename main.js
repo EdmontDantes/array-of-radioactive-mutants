a2f6a32367c933bfc27f69c42764a7ea786a1bce
Bogdan Kowaltchook <bogdan.kowaltchook@codeimmersives.com>
2020-02-24 13:48:17 -0500

    passed 6 out of 6 total

--- main.js
+++ main.js
@@ -1,11 +1,45 @@
 /******************
  * YOUR CODE HERE *
  ******************/
+function addMeToEnd (arr) {
+  arr.push('Colin');
+  return arr;
+}
 
+function addMeToStart (arr) {
+  arr.unshift('Colin');
+  return arr;
+}
 
+function changeLast (arr, valueLast) {
+  arr.pop();
+  arr.push(valueLast);
+  return arr;
+}
 
+function changeAllValuesTo (arr, valuesAll) {
+  for(let i = 0; i < arr.length; i++) {
+    arr[i] = valuesAll;
+  }
+  return arr;
+}
 
+function oddOrEven (arr) {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] % 2 === 0){
+      arr.splice(i, 1, 'even');
+    }else {
+      arr.splice(i, 1, 'odd');
+    }
+  }
+  return arr;
+}
 
+function changeNextThreeToValue (givenIndex, arr, givenValue) {
+  // for (let i = 0; i < arr.length; i++) {
+  return arr.splice(givenIndex, 3, givenValue, givenValue, givenValue);
+  // }
+}
 /*********************************
  * OUR CODE BELOW; DO NOT TOUCH! *
  *********************************/
